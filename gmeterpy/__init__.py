e432101a5f64855a600fecc6a138d3ccaf19f630
Ilya Oshchepkov <ilya.oshchepkov@gmail.com>
2023-10-27 18:05:01 +0300

    Update dependencies

--- gmeterpy/__init__.py
+++ gmeterpy/__init__.py
@@ -2,6 +2,5 @@
 
 """Top-level package for gMeterPy."""
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
