9bdf6bf4b2ee7acb4eb97d769bc4a65d50b44626
Ilya Oshchepkov <ilya.oshchepkov@gmail.com>
2023-12-10 00:51:19 +0300

    Major update for vertical gravity gradient fit

--- scripts/vgfit.py
+++ scripts/vgfit.py
@@ -9,43 +9,39 @@ from gmeterpy.corrections.vgrad import fit_floating_gravity
 from gmeterpy.plotting.vgfit import plot_fit
 from gmeterpy.corrections.vgrad import generate_report
 
-parser = argparse.ArgumentParser(description='Second-order polynomial fit of the vertical gravity gradients')
-parser.add_argument('-i', metavar='in-file', type=argparse.FileType('rt'), required=True)
-parser.add_argument('-n', '--name', default='')
+parser = argparse.ArgumentParser(
+        description='Polynomial fit of the vertical gravity gradients')
+parser.add_argument('-i', '--input', type=argparse.FileType('rt'),
+                    required=True, help='Input file')
+parser.add_argument('-n', '--name', default='', type=str,
+                    help='Station name')
+parser.add_argument('-d', '--degree', type=int, default=2,
+                    help='Degree of the polynomial model')
 
-#parser.add_argument('--plot', dest='plot', action='store_true')
-#parser.add_argument('--no-plot', dest='plot', action='store_false')
-#parser.set_defaults(plot=True)
+args = parser.parse_args()
 
-opt = parser.parse_args()
+data = read_csv(args.input)
 
-data = read_csv(opt.i)
+df, res = fit_floating_gravity(data, deg=args.degree)
+params = res.params.iloc[-args.degree:]
+cov_params = res.cov_params().iloc[-args.degree:,-args.degree:]
 
-df, res = fit_floating_gravity(data, deg=2)
-a, b = res.params.iloc[-2], res.params.iloc[-1]
-
-se_a = res.bse.iloc[-2]
-se_b = res.bse.iloc[-1]
-covab = res.cov_params()['a']['b']
 df['resid'] = res.resid
 
-gp = np.poly1d([b, a, 0])
+gp = np.poly1d(
+        np.concatenate([params.values[::-1], np.zeros(1)]),
+        variable='h')
 
 make_plot = True
 make_report = True
 
-station = opt.name
+station = args.name
 
 if make_plot:
-    h_min = min(df.h)
-    al = (gp(h_min) - gp(1.0)) / (h_min - 1.0)
-    fig = plot_fit(df, lambda x: gp(x) - al*x, (se_a, se_b, covab), station)
-    title = '{} ({:.1f} $\mu Gal/m$  substructed)'.format(station, al)
-    fig.gca().set_title(title, fontsize=14)
+    fig = plot_fit(df, gp, cov_params, station)
     plot_file = station + '_all.png'
     fig.savefig(plot_file)
 
 if make_report:
     report_file = 'vg_' + station + '_fit_all.txt'
-    generate_report(report_file, data, res, gp, (se_a, se_b, covab), station)
-
+    generate_report(report_file, data, res, gp, station)
